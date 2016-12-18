70ada2d16eb2b5406bf54ee112df3e72b5565271
serj <serj.stratu@gmail.com>
2016-12-18 21:55:43 +0200

    fixes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,12 @@
 $( window ).load(function() {
+    $(".box").hide();
     let link = $(".left-nav li.active a").data("id");
-    console.log(link);
 
     var activeBox = $('.box').filter(function() {
         return $(this).data("category") == link;
     });
-    console.log(activeBox)
     $(activeBox).show();
 });
-
-
-
-// $( document ).ready(function() {
-//     var heights = $(".wrap").map(function() {
-//             return $(this).height();
-//         }).get(),
-//
-//         maxHeight = Math.max.apply(null, heights);
-//
-//     $(".left-nav").height(maxHeight);
-// });
 
 
 
