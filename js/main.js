dcfd37a4c423f4cd413febc41fc9892227a76a04
serj <serj.stratu@gmail.com>
2016-12-18 17:08:08 +0200

    right side finish + responsive layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+$( document ).ready(function() {
+    var heights = $(".wrap").map(function() {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights);
+
+    $(".wrap").height(maxHeight);
+});
+
+
 $(".left-nav").on("click", "a", function(e){
     e.preventDefault();
     console.log("click a");
